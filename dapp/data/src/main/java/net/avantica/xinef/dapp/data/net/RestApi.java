d1270906548a686527b31dc6f5bd4e30d9eb5f9d
LIM-WS01265\Edward <edward.carrion@avantica.net>
2017-01-04 15:27:09 -0500

    Constant names in RestApi are more specific

--- dapp/data/src/main/java/net/avantica/xinef/dapp/data/net/RestApi.java
+++ dapp/data/src/main/java/net/avantica/xinef/dapp/data/net/RestApi.java
@@ -8,13 +8,14 @@ import java.util.List;
 import rx.Observable;
 
 public interface RestApi {
-    String API_URL_GET_PUBLIC_INVESTMENT_PROJECT_LIST = BuildConfig.HOST + "datastreams/PIP-GEORE-DE-GOBIE-LOCAL/data.json/?auth_key=" + BuildConfig.AUTH_KEY + "&limit=50";
-    String API_URL_GET_PUBLIC_INVESTMENT_PROJECT_DETAIL = BuildConfig.HOST + "datastreams/PIP-GEORE-DE-GOBIE-LOCAL/data.json/?auth_key=" + BuildConfig.AUTH_KEY + "&limit=2";
+    String API_URL_GET_PUBLIC_INVESTMENT_PROJECT_LIST = BuildConfig.HOST_MEF + "datastreams/PIP-GEORE-DE-GOBIE-LOCAL/data.json/?auth_key=" + BuildConfig.AUTH_MEF_KEY + "&limit=50";
+    String API_URL_GET_PUBLIC_INVESTMENT_PROJECT_LIST_BY_DEPARTMENT = BuildConfig.HOST_MEF + "datastreams/PIP-GEORE-DE-GOBIE-LOCAL/data.json/?auth_key=" + BuildConfig.AUTH_MEF_KEY + "&query=&qtype=column0&filter0=column0%5B%3D%3D%5D";
+    String API_URL_GET_PUBLIC_INVESTMENT_PROJECT_DETAIL = BuildConfig.HOST_MEF + "datastreams/PIP-GEORE-DE-GOBIE-LOCAL/data.json/?auth_key=" + BuildConfig.AUTH_MEF_KEY + "&limit=2";
 
     /**
      * Retrieves an {@link Observable} which will emit a List of {@link PublicInvestmentProjectEntity}.
      */
-    Observable<List<PublicInvestmentProjectEntity>> publicInvestmentProjectEntityList();
+    Observable<List<PublicInvestmentProjectEntity>> publicInvestmentProjectEntityList(String departmentName);
 
     /**
      * Retrieves an {@link Observable} which will emit a {@link PublicInvestmentProjectEntity}.
